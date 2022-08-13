eda6ee6bf7c7049bebd1f2b39b798adb37e6464f
ViktorPopov44 <110253043+ViktorPopov44@users.noreply.github.com>
2022-08-13 10:19:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+int[] TableSquares(int s)
+{
+    int[] array = new int[s];
+
+    for (int i = 1; i <= n; i++)
+    {
+        array[i - 1] = i * i;
+    }
+    return array;
+}
+int[] result = TableSquares(n);
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
